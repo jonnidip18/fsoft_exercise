84180dcecff3b689bc00c03e40ac85ec62913ff0
Phuong Hong Nguyen <jonnidip18@gmail.com>
2022-04-20 14:18:40 +0700

    add consumer and producer

--- Queue_exercise2/src/main/java/Queue_consumer.java
+++ Queue_exercise2/src/main/java/Queue_consumer.java
@@ -1,20 +1,15 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.Calendar;
-import java.util.PriorityQueue;
-
 import org.redisson.Redisson;
 import org.redisson.api.RQueue;
 import org.redisson.api.RedissonClient;
 
-public class Queue_producer {
+import java.io.IOException;
+
+public class Queue_consumer {
     public static void main(String... args) throws IOException {
         RedissonClient redisson = Redisson.create();
 
         RQueue<String> queue = redisson.getQueue("myQueue");
 
-        queue.add(Calendar.getInstance().getTime().toString());
-
-        System.out.println(queue);
+        System.out.println(queue.poll());
     }
 }
